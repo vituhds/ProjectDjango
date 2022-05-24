1c0fd486df5809a83280301d559cf1bec5f28a0e
Matheus Montagna <matheusmontagna123@gmail.com>
2022-05-24 12:37:07 -0300

    sla eu

--- siteHome/models.py
+++ siteHome/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 #   classe padrão todos  os models vão ter esta configurações.
 
 
@@ -11,14 +12,14 @@ class Base(models.Model):
     class Meta:
         abstract = True
 
+
 class PaginaInicial(Base):
-    Titulo_Header = models.CharField('Titulo', max_length=100, blank=True)
-    SubTitulo_Header = models.CharField('SubTítulo', max_length=100, blank=True)
+    Titulo_Header01 = models.CharField('Titulo', max_length=100, blank=True)
+    SubTitulo_Header01 = models.CharField('SubTítulo', max_length=100, blank=True)
 
     class Meta:
         verbose_name = 'PaginaInicial'
         verbose_name_plural = 'PaginasIniciais'
 
-        def __str__ (self):
+        def __str__(self):
             return self.Titulo_Header
-
