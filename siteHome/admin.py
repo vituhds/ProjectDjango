b4d3c7c61df06e5beacb09611b46929ab230a22f
Rodrigospicker1 <r.spicker13@gmail.com>
2022-05-26 10:27:18 -0300

    qualquercoisa

--- siteHome/admin.py
+++ siteHome/admin.py
@@ -7,5 +7,5 @@ from siteHome.models import PaginaInicial
 
 @admin.register(PaginaInicial)
 class PaginaInicialsAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'subTitulo', 'Criados', 'Modificado', 'Ativo')
+    list_display = ('titulo', 'subTitulo', 'treinamento', 'Ativo')
 
